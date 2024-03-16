730e17624bf0812bce6159b33415659e7cfa72a9
reza-ya-57 <rezaayaari.57@gmail.com>
2024-03-16 15:19:54 +0330

    chore: change folder structure of test and program

---
+++ Code-Challange-CSharp-Test/LeetCode/CourseSchedule/CourseScheduleTest.cs
@@ -0,0 +1,23 @@
+﻿using Code_Challange_CSharp.LeetCode;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challange_CSharp_Test.LeetCode
+{
+    public static class CourseScheduleTest
+    {
+        [Fact]
+        public static void CanFinishTest()
+        {
+            var test = new int[1][];
+            test[0] = new int[] { 1, 0 };
+            test[1] = new int[] { 0, 1 };
+            var result = CourseSchedule.CanFinish(2, test);
+            Assert.False(result);
+        }
+    }
+}
