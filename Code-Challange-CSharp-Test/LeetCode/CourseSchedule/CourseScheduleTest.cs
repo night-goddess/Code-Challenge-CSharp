f460913a7a305a798dae1ad8838e260cd7e2c370
reza-ya-57 <rezaayaari.57@gmail.com>
2024-03-17 18:50:08 +0330

    chore: complete config of testing

--- Code-Challange-CSharp-Test/LeetCode/CourseSchedule/CourseScheduleTest.cs
+++ Code-Challange-CSharp-Test/LeetCode/CourseSchedule/CourseScheduleTest.cs
@@ -1,6 +1,7 @@
 ﻿using Code_Challange_CSharp.LeetCode;
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,27 @@ namespace Code_Challange_CSharp_Test.LeetCode
 {
     public static class CourseScheduleTest
     {
-        [Fact]
-        public static void CanFinishTest()
+        [Theory]
+        [ClassData(typeof(CourseScheduleSampleData))]
+        public static void CanFinishTest(int numCourse , int[][] prerequisites , bool expected)
         {
-            var test = new int[1][];
-            test[0] = new int[] { 1, 0 };
-            test[1] = new int[] { 0, 1 };
-            var result = CourseSchedule.CanFinish(2, test);
-            Assert.False(result);
+            var result = CourseSchedule.CanFinish(numCourse, prerequisites);
+            Assert.Equal(result , expected);
         }
+    }
+
+
+
+    public class CourseScheduleSampleData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            int[][] test = new int[1][];
+            test[0] = [1 , 2];
+            yield return new object[] { 2, test  , false};
+            //yield return new object[] { 1, [[1 ,2]] };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
